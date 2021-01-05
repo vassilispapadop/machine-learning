1abc53720039b133fc51771723533eee2ab9fb7f
Vassilis <vassilispapadop@gmail.com>
2021-01-05 14:32:45 +0200

    some comments

--- fundamentals/simple_linear_regression.m
+++ fundamentals/simple_linear_regression.m
@@ -62,12 +62,16 @@ iterations = 1500;
 w_current =0;
 b_current = 0;
 for num_iterations = 1:iterations
-    k=1:m
-    j1=(1/m)*sum((theta(1)+theta(2).*x2(k,2))-y(k));
-    j2=(1/m)*sum(((theta(1)+theta(2).*x2(k,2))-y(k)).*x2(k,2));
+    k=1:m;
+    % y- prediction
+    y_current = theta(1) + theta(2) .* x2(k,2);
+    % current cost of prediction
+    cost_current = y_current - y(k);
+    j1=(1/m)*sum(cost_current);
+    j2=(1/m)*sum((cost_current).*x2(k,2));
+    % update theta
     theta(1)=theta(1)-alpha*(j1);
-    theta(2)=theta(2)-alpha*(j2); 
-    
+    theta(2)=theta(2)-alpha*(j2);  
 end
 
 
